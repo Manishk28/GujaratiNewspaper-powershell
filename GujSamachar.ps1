d1fa3f978e624a7206860fb9e6f2df9134805115
Manish Kothari <manish.h.kothari@gmail.com>
2022-08-06 17:41:43 +0530

    New scripts for newspapers

---
+++ GujSamachar.ps1
@@ -0,0 +1,45 @@
+﻿# Determine script location for PowerShell
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$targetDir = "$DesktopPath\Newspapers\"
+$GSDir = "$DesktopPath\Newspapers\Gujarat_samachar\"
+
+# Download function.
+function DownloadFile([Object[]] $sourceFiles,[string]$targetDirectory) {            
+ $wc = New-Object System.Net.WebClient            
+    $i=0         
+ foreach ($sourceFile in $sourceFiles){            
+  $sourceFileName = $sourceFile.SubString($sourceFile.LastIndexOf('/')+1)            
+  $targetFileName = $targetDirectory + "Newpaper_page" + $i.ToString('00') + ".jpg"       
+  $wc.DownloadFile($sourceFile, $targetFileName)       
+  Write-Host "Downloaded $sourceFile to file location $targetFileName"
+  $i++          
+ }            
+            
+}
+
+# create folder if not exists
+if (!(test-path -path $GSDir)) {new-item -path $GSDir -itemtype directory}
+
+
+# Creating filename list for gujarat samachar.
+$GSPaperlist = new-object -TypeName "System.Collections.ArrayList"
+
+$WebResponse = Invoke-WebRequest "https://epaper.gujaratsamachar.com/mumbai/06-08-2022/1"
+$srcrpattern = '(?i)src="(.*?)"'
+
+$src = ([regex]$srcrpattern).Matches($WebResponse.RawContent) | ForEach-Object { $_.Groups[1].Value }
+
+ForEach($value in $src){
+    If($value -match "https://epaperstatic.gujaratsamachar.com/epaper/thumbnail/"){ 
+        $link = $value
+        $link = $link -replace "/thumbnail/", "/"
+        $GSPaperlist.Add($link)
+    }
+}
+
+DownloadFile $GSPaperlist $GSDir 
+
+
+
---
+++ MumSamachar.ps1
@@ -0,0 +1,53 @@
+﻿# Determine script location for PowerShell
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$targetDir = "$DesktopPath\Newspapers\"
+$MSDir = "$DesktopPath\Newspapers\Mumbai_samachar\"
+
+# Download function.
+function DownloadFile([Object[]] $sourceFiles,[string]$targetDirectory) {            
+ $wc = New-Object System.Net.WebClient            
+    $i=0         
+ foreach ($sourceFile in $sourceFiles){            
+  $sourceFileName = $sourceFile.SubString($sourceFile.LastIndexOf('/')+1)            
+  $targetFileName = $targetDirectory + "Newpaper_page" + $i.ToString('00') + ".pdf"       
+  $wc.DownloadFile($sourceFile, $targetFileName)            
+  Write-Host "Downloaded $sourceFile to file location $targetFileName"
+  $i++          
+ }            
+            
+}
+
+# create folders if not exists
+if (!(test-path -path $MSDir)) {new-item -path $MSDir -itemtype directory}
+
+# set date
+$date = Get-Date
+
+# set date mumbai samachar
+$datems1 = $date.ToString("dd-MM-yyyy")
+$datems2 = $date.ToString("ddMMyy")
+
+# Creating filename list for mumbai samachar.
+$MSPaperlist = new-object -TypeName "System.Collections.ArrayList"
+
+$WebResponse = Invoke-WebRequest "https://bombaysamachar.com/%E0%AA%87-%E0%AA%AA%E0%AB%87%E0%AA%AA%E0%AA%B0/"
+$filteredHTML = ($WebResponse.ParsedHtml.body.getElementsbyClassName('_df_thumb '))
+$pattern = 'thumb="(.*).jpg'
+foreach ($element in $filteredHTML)
+{
+    If(([regex]::match($element.outerHTML, $pattern).Groups[1].Value) -match $datems2){
+        $link = [regex]::match($element.outerHTML, $pattern).Groups[1].Value + ".pdf"
+        $MSPaperlist.Add($link)
+    }
+}
+
+DownloadFile $MSPaperlist $MSDir
+
+
+#merge pdfs
+Start-Process -FilePath "$ScriptDir\PDFtk\pdftk.exe" -ArgumentList "Mumbai_samachar\*.pdf cat output MumbaiSamachar.pdf" -WorkingDirectory $targetDir -wait
+
+#delete each directory.
+Remove-Item $MSDir -Recurse
